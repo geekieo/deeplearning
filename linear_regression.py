c7ff089b92635ec596ce46a36c0b8c7be7ca7e71
geekieo <geekieo@hotmail.com>
2017-08-18 03:21:35 +0800

    线性回归类

---
+++ linear_regression.py
@@ -0,0 +1,10 @@
+from perceptron import Perceptron
+from activator import linear
+
+class LinearRegssion(Perceptron):
+    def __init__(self, input_num):
+        '''
+        初始化线性回归模型，设置输入参数的个数
+        '''
+        # 感知机取消阶跃激活函数即为线性回归
+        Perceptron.__init__(self, input_num, linear)
