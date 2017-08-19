ba170f3cb9d9944bce9137e56c7641578bbacc43
geekieo <geekieo@hotmail.com>
2017-08-20 00:12:32 +0800

    0均值标准差归一化

--- normilaztion.py
+++ normilaztion.py
@@ -1,4 +1,5 @@
 from typeError import TypeError
+from math import sqrt
 
 class Normalization():
     '''
@@ -12,14 +13,16 @@ class Normalization():
         '''
         # inX 为待处理数组
         self.inX = inX
+        self.length = len(inX)
         self.correctFormat = self._check()  # 校验 inX 格式
         self.mean = self._mean()
         self.variance = self._variance()
+        self.stdDeviation = self._stdDeviation()
         self.maxValue, self.minValue = self._endValue()
 
     def _check(self):
         '''格式校验'''
-        if type(self.inX).__name__ != 'list' or len(self.inX) == 0:
+        if type(self.inX).__name__ != 'list' or self.length == 0:
             raise TypeError("Please check the value, make sure it is a list!")
 
     def _endValue(self):
@@ -39,7 +42,7 @@ class Normalization():
         sig = 0
         for i in self.inX:
             sig += i
-        return sig / len(self.inX)
+        return sig / self.length
 
     def _variance(self):
         '''
@@ -48,21 +51,44 @@ class Normalization():
         var = 0
         for i in self.inX:
             var += (i - self.mean)**2
-        return var
+        return var / self.length
 
-    def linearNorm(self):
+    def _stdDeviation(self):
         '''
-        线性归一化,把一组数值线性映射到[0,1] 区间
-        计算公式：inX_norm = (inX - inX_min)/(inX_max-inXmin)
-        返回：归一化数组
+        求标注差
+        '''
+        return sqrt(self.variance)
+
+    def minMaxNorm(self):
+        '''
+        最大最小值归一化
+        把一组数值线性映射到 [0,1] 区间
+        计算公式：x_norm = (x - inX_min) / (inX_max - inX_min)
+        返回：归一化的数组
+        适用环境：不需要考虑特征分量之间的量纲差异，适用于大部分情况
         '''
         delta = self.maxValue - self.minValue
         return list(map(lambda x: (x - self.minValue) / delta, self.inX))
+    
+    
+    def zScoreNorm(self):
+        '''
+        0均值标准差归一化
+        把一组数组映射到均值为0的标准差归一化区间
+        计算公式：x_norm = (x - inX_mean) / stdDeviation
+        返回：归一化数组
+        适用环境：需要消除特征分量之间量纲差异
+        '''
+        return list(map(lambda x:(x-self.mean)/self.stdDeviation, self.inX))
+
+
 
 def test():
     x = [2, 4, 6, 7, 8]
     n = Normalization(x)
-    linearNorn = n.linearNorm()
+    minmaxnorm = n.minMaxNorm()
+    zscorenorm = n.zScoreNorm()
     print(x)
-    print(n.mean)
-    print(linearNorn)
+    print(n.mean, n.stdDeviation)
+    print(minmaxnorm)
+    print(zscorenorm)
