8db3b24296af7809d8de3e2c566b1a8d558251d4
geekieo <geekieo@hotmail.com>
2017-08-21 17:16:14 +0800

    线性回归例程加入归一化

--- linear_regression_sample.py
+++ linear_regression_sample.py
@@ -1,5 +1,5 @@
 from linear_regression import LinearRegssion
-
+from normalization import Normalization
 
 def get_train_dataset():
     '''
@@ -25,23 +25,20 @@ def get_train_dataset():
     return input_vecs, labels
 
 
-def train_linear_regression():
-    '''
-    使用数据训练线性回归模型
-    '''
+if __name__ == '__main__':
+    '''训练并使用线性回归模型'''
     # 创建感知器，输入参数的特征数为1
     lr = LinearRegssion(1)
+    train_vecs, labels = get_train_dataset()
+    # 训练样本归一化
+    # 注意: 后面的测试样本也得用 train_norm 归一化
+    train_norm = Normalization(train_vecs)
+    input_vecs = train_norm.minMaxNorm(train_vecs)
     # 训练，迭代10轮，学习速率为0.01
-    input_vecs, labels = get_train_dataset()
-    lr.train(input_vecs, labels, 100, 0.0001)
-    # 返回训练好的线性回归模型
-    return lr
-
-
-if __name__ == '__main__':
-    '''训练线性回归模型'''
-    lr = train_linear_regression()
+    lr.train(input_vecs, labels, 100, 0.01)
     # 打印训练获得的权重
     print(lr)
     # 测试
-    print('area = 95, price = %.2f ' % lr.predict([95]))
+    test_vecs = [95]
+    test_vecs = train_norm.minMaxNorm(test_vecs)
+    print('area = 95, price = %.2f ' % lr.predict(test_vecs))
