2f9d95a88d20a3ff83306d82f51b8d1b78756678
geekieo <geekieo@hotmail.com>
2017-08-27 04:06:36 +0800

    setuptools 生成包文件，使模块可以跨文件夹引用

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(  
+    name="deeplearning",
+    version="0.1",
+    packages=find_packages(),
+    )
+
+# 执行 python setup.py bdist_egg 即可将这个文件夹打包
