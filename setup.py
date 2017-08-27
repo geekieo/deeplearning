ecd97620c7e91617dacee41680a7e8c7859d7d58
geekieo <geekieo@hotmail.com>
2017-08-27 23:47:34 +0800

    setuptools

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 setup(  
-    name="deeplearning",
-    version="0.1",
-    packages=find_packages(),
+    name = "deeplearning",
+    version = "0.1",
+    packages = find_packages(),
+
+    author = "geekieo",
+    author_email = "geekieo@hotmail.com",
     )
 
-# 执行 python setup.py bdist_egg 即可将这个文件夹打包
+# step 1: 执行 python setup.py bdist_egg 即可将这个文件夹打包
+# step 2: 执行 python setup.py install 将 egg 安装到 python 的 Lib/site-package
