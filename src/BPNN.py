200e2330cc5b11c0ad5e8f8621dd78b8144fe6eb
geekieo <geekieo@hotmail.com>
2017-09-13 01:52:34 +0800

    添加常数项节点类

--- src/BPNN.py
+++ src/BPNN.py
@@ -39,10 +39,8 @@ class Node(object):
         '''
         self.layer_index = layer_index
         self.node_index = node_index
-        #前层节点
-        self.f_stream = []
-        #后层节点
-        self.b_stream = []
+        self.f_stream = [] #前层节点
+        self.b_stream = [] #后层节点
         self.output = 0
         self.error = 0
 
@@ -53,13 +51,13 @@ class Node(object):
         '''
         self.output = output
 
-    def append_forwardstream_connection(self, conn):
+    def append_f_stream_connection(self, conn):
         '''
         添加一个到前一层节点的链接
         '''
         self.f_stream.append(conn)
 
-    def append_backstream_connection(self, conn):
+    def append_b_stream_connection(self, conn):
         '''
         添加一个到后一层节点的链接
         '''
@@ -87,19 +85,20 @@ class Node(object):
     def calc_hidden_layer_error(self):
         '''
         计算隐层节点误差梯度
-        隐层BP公式：error = output*(1-output)*Σ(W·out_errors)
+        隐层BP公式：error = output*(1-output)*Σ(w·out_error)
             error 为当前节点方向传播误差值；output 为当前节点前馈输出值；
-            W 为当前节点前馈权重，多个权重；out_errors 为当前节点前馈节点误差，多个误差；
+            W 为当前节点前馈权重，有多个权重；out_error 为当前节点前馈节点误差，对应权重，有多个误差；
             当前节点的误差为前馈层的节点误差加权和，不同节点的前馈节点误差相同，但不同节点的前馈权重不同。
         '''
         # ret 为 reduce 时的加权项缓存
         b_stream_error = reduce(
             lambda ret, conn: ret + conn.b_stream_node.error * conn.weight,
             self.b_stream, 0.0)
+        self.error = self.output *(1-self.output)*b_stream_error
 
     def __str__(self):
         '''
-        重载对象打印调用的方法，打印节点的信息
+        重写对象打印方法，打印节点的信息
         '''
         node_str = 'L%u-N%u: output: %f error: %f' % (self.layer_index,
                                                       self.node_index,
@@ -109,3 +108,48 @@ class Node(object):
         f_stream_str = reduce(lambda ret, conn: ret + '\n\t' + str(conn),
                               self.f_stream, '')
         return node_str + '\n\tb_stream:' + b_stream_str + '\n\tf_stream:' + f_stream_str
+
+
+class ConstNode(object):
+    '''
+    常数项节点
+    实现一个输出恒为1的节点，计算偏执项 wb 时需要
+    1 的地位等同于其他节点输入的特征变量 x  
+    公式 bias_output = 1*wb
+    '''
+
+    def __init__(self, layer_index, node_index):
+        '''
+        构造节点对象
+        layer_index: 节点所属层的编号
+        node_index: 节点的编号
+        '''
+        self.layer_index = layer_index
+        self.node_index = node_index
+        self.b_stream = [] #后层节点
+        self.output = 1
+        self.error = 0
+
+    def append_b_stream_connection(self, conn):
+        '''
+        添加一个到下一层节点的链接
+        '''
+        self.b_stream.append(conn)
+
+    def calc_hidden_layer_error(self):
+        '''
+        计算隐层节点误差梯
+        隐层BP公式：error = output*(1-output)*Σ(w·out_error)
+        '''
+        b_stream_error = reduce(
+            lambda ret, conn: ret + conn.b_stream_node.error * conn.weight,
+            self.b_stream, 0.0)
+        self.error = self.output * (1 - self.output) * b_stream_error
+   
+    def __str__(self):
+        '''
+        打印节点的信息
+        '''
+        node_str = '%u-%u: output: 1' % (self.layer_index, self.node_index)
+        b_stream_str = reduce(lambda ret, conn: ret + '\n\t' + str(conn), self.b_stream, '')
+        return node_str + '\n\tb_stream:' + b_stream_str
